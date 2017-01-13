c8b71ebb2f82c6c6b1a74c710966d637c68179de
Tuffk <tuffkmulhall@gmail.com>
2017-01-13 08:55:21 -0600

    add pointers

--- P1/unitarias/c2/tes_almacen.cc
+++ P1/unitarias/c2/tes_almacen.cc
@@ -6,26 +6,27 @@ class almacenFixture : public ::testing::Test
 public:
 
   void SetUp(void){
-    construct(v,10);
+    v = new vector<int>();
+    construct(*v,10);
   }
 
   void TearDown(void){
-
+    delete v;
   }
 
-  std::vector<int> v;
+  std::vector<int> * v;
 };
 
 TEST_F(almacenFixture, construyemela){
-  ASSERT_EQ(10,v.size());
+  ASSERT_EQ(10,v->size());
 }
 
 TEST_F(almacenFixture,agregamela){
-  agregamela(v,2);
-  ASSERT_EQ(11,v.size());
+  agregamela(*v,2);
+  ASSERT_EQ(11,v->size());
 }
 
 TEST_F(almacenFixture, borramela){
-  borramela(v,2);
-  ASSERT_EQ(9,v.size());
+  borramela(*v,2);
+  ASSERT_EQ(9,v->size());
 }
