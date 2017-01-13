848414e031172012e0f537dcf6e66bcd6666c59b
Tuffk <tuffkmulhall@gmail.com>
2017-01-13 09:51:06 -0600

    hol aareial calse 2

--- P1/unitarias/c2/tes_almacen.cc
+++ P1/unitarias/c2/tes_almacen.cc
@@ -1,28 +1,40 @@
 #include "/home/tuff/github/Calidad-Y-Pruebas/googletest/googletest/include/gtest/gtest.h"
 #include "almacen.cc"
+#include <ctime>
 
-class almacenFixture : public ::testing::Test
+class timeFixture : public testing::Test
+{
+  public:
+  virtual void SetUp(){
+    tiempo = time(NULL);
+  }
+
+  virtual void TearDown(){
+    EXPECT_TRUE((time(NULL) - tiempo) <= 2) << "tardo mucho \n";
+  }
+  private:
+  time_t tiempo;
+};
+
+class almacenFixture : public timeFixture
 {
 public:
 
   void SetUp(void){
+    timeFixture::SetUp();
     v = new vector<int>();
     construct(*v,10);
   }
 
   void TearDown(void){
     delete v;
-  }
-  void SetUpTestCase()
-  {
-    //general para todos los cases
-  }
-  void TearDownTestCase(){
-
+    timeFixture::TearDown();
   }
 
   std::vector<int> * v;
 };
+
+
 
 TEST_F(almacenFixture, construyemela){
   ASSERT_EQ(10,v->size());
@@ -36,4 +48,9 @@ TEST_F(almacenFixture,agregamela){
 TEST_F(almacenFixture, borramela){
   borramela(*v,2);
   ASSERT_EQ(9,v->size());
+}
+
+
+TEST_F(almacenFixture, fail){
+  sleep(3);
 }
