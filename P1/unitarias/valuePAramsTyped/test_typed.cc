b2937d832a0eaa21117c4d0cd59c47e17c654a3b
Tuffk <tuffkmulhall@gmail.com>
2017-01-17 08:20:39 -0600

    inicio typed testing

---
+++ P1/unitarias/valuePAramsTyped/test_typed.cc
@@ -0,0 +1,8 @@
+#include "/home/tuff/github/Calidad-Y-Pruebas/googletest/googletest/include/gtest/gtest.h"
+#include "functions.h"
+
+template <class T>
+class fixTT : public testing::Test
+{
+
+};
--- P1/unitarias/valuePAramsTyped/test_valueParams.cc
+++ P1/unitarias/valuePAramsTyped/test_valueParams.cc
@@ -33,4 +33,7 @@ TEST_P(fixValParam, odds){
 
 //INSTANTIATE_TEST_CASE_P(vals,fixValParam,testing::Values(2,4,6,8,1,3,5,7,9));
 
-INSTANTIATE_TEST_CASE_P(vals,fixValParam,testing::Range(1,25,1));
+//INSTANTIATE_TEST_CASE_P(vals,fixValParam,testing::Range(1,25,1));
+
+int arr[] = {1,2,3,4,5,6,7,8,9};
+INSTANTIATE_TEST_CASE_P(vals,fixValParam,testing::ValuesIn(arr));
