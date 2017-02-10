e82b8f0914218e07974aae4dedf70c443baeaed5
Tuffk <tuffkmulhall@gmail.com>
2017-02-10 08:58:40 -0600

    npi

--- P2/codeWithLeaks/VideoJuego.h
+++ P2/codeWithLeaks/VideoJuego.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include<string>
+#include<iostream>
 #include"ClonTemplate.h"
 
 using namespace std;
@@ -16,12 +17,13 @@ public:
     }
     VideoJuego(const VideoJuego & old)
     {
-        this->nombre = new string();
-        *(this->nombre) = *(old.nombre);
-        this->num_serie = new int();
-        *(this->num_serie)= *(old.num_serie);
-        this->tipo_juego = new tipo();
-        *(this->tipo_juego)=*(old.tipo_juego);
+        this->nombre = new string((*(old.nombre)));
+        //*(this->nombre) = *(old.nombre);
+        this->num_serie = new int((*(old.num_serie)));
+        //*(this->num_serie)= *(old.num_serie);
+        this->tipo_juego = new tipo((*(old.tipo_juego)));
+        //*(this->tipo_juego)=*(old.tipo_juego);
+        cout << "copy VJ" << endl;
 
     }
     string getName()
@@ -46,15 +48,16 @@ class Estrategia : public ClonTemplate<VideoJuego, Estrategia>
 public:
     Estrategia(string nombre, int num_serie, VideoJuego::tipo tipo_juego, int number_of_players)
     {
-        this->nombre = new string(nombre);
-        this->num_serie = new int(num_serie);
-        this->tipo_juego = new VideoJuego::tipo(tipo_juego);
+        //this->nombre = new string(nombre);
+        //this->num_serie = new int(num_serie);
+        //this->tipo_juego = new VideoJuego::tipo(tipo_juego);
         this->number_of_players = new int(number_of_players);
     }
     Estrategia(const Estrategia& old)
     {
-        this ->number_of_players = new int();
-        *(this->number_of_players) = *(old.number_of_players);
+        this ->number_of_players = new int((*(old.number_of_players)));
+        //*(this->number_of_players) = *(old.number_of_players);
+        cout << "copy estra" << endl;
     }
 
     ~Estrategia()
@@ -70,9 +73,9 @@ class Aventura : public ClonTemplate<VideoJuego, Aventura>
 public:
     Aventura(string nombre, int num_serie, VideoJuego::tipo tipo_juego, string rate_category)
     {
-        this->nombre = new string(nombre);
-        this->num_serie = new int(num_serie);
-        this->tipo_juego = new VideoJuego::tipo(tipo_juego);
+        // this->nombre = new string(nombre);
+        // this->num_serie = new int(num_serie);
+        // this->tipo_juego = new VideoJuego::tipo(tipo_juego);
         this->rate_category = new string(rate_category);
     }
     ~Aventura()
@@ -88,9 +91,9 @@ class Aprendizaje : public ClonTemplate<VideoJuego, Aprendizaje>
 public:
     Aprendizaje(string nombre, int num_serie, VideoJuego::tipo tipo_juego, int num_niveles)
     {
-        this->nombre = new string(nombre);
-        this->num_serie = new int(num_serie);
-        this->tipo_juego = new VideoJuego::tipo(tipo_juego);
+        // this->nombre = new string(nombre);
+        // this->num_serie = new int(num_serie);
+        // this->tipo_juego = new VideoJuego::tipo(tipo_juego);
         this->num_niveles = new int(num_niveles);
     }
     ~Aprendizaje()
