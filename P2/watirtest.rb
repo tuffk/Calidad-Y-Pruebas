66088bd51d42843dc19e0e86f5a0e83c9c2b50cf
Tuffk <tuffkmulhall@gmail.com>
2017-03-24 08:40:38 -0600

    mac apests

--- P2/watirtest.rb
+++ P2/watirtest.rb
@@ -1,4 +1,24 @@
+# ruby watirtest.rb
 require 'rubygems'
 require 'watir'
 browser = Watir::Browser.new
+browser.goto 'http://bit.ly/watir-example'
+
+browser.text_field(:name => 'entry.1000000').set 'monzalo es cancer'
+puts 'algo que aparece'
+puts browser.text_field(:name => 'entry.1000000').value
+zain = ""
+for i in 1..10 do
+    zain = zain + "hola iter #{i} \n"
+end
+
+browser.textarea(:id => 'entry_1000001').set zain
+
+browser.textarea(:css => 'textarea[name="entry.1000001"]').clear
+
+browser.element(:xpath => '//textarea[@name="entry.1000001"]').send_keys "hola mac apesta"
+#set ({"text" => "hola"})
+
+
+sleep(1)
 browser.close
